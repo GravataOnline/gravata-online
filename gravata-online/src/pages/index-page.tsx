d2f9a432b6531ab414cec4adafa653987cff5970
Barbara Hellen <barbara.pereira@modalgr.com.br>
2022-04-17 18:08:48 -0300

    inicializando home page

--- gravata-online/src/pages/index-page.tsx
+++ gravata-online/src/pages/index-page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image";
 
 export function IndexPage() {
   return (
-    <>
-      {/* <Image src={"./shrek.jpg"} width={14} height={14} /> */}
-      <img src="./shrek.jpg" alt="" />
-      <h1>23</h1>
-    </>
+    <div className="">
+      <section>
+        <div className="w-full h-[100px] bg-green-600 ">a</div>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </div>
   );
 }
