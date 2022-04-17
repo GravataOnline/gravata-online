891f833e1ffc2c82309bbe738839447ccac54a65
Barbara Hellen <barbara.pereira@modalgr.com.br>
2022-04-17 17:29:28 -0300

    excluindo arquivos de teste

--- gravata-online/tailwind.config.js
+++ gravata-online/tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: (theme) => ({
-        "hero-pattern": "url('/img/hero-pattern.svg')",
-        "footer-texture": "url('/img/footer-texture.png')",
-      }),
+      // backgroundImage: (theme) => ({
+      //   "hero-pattern": "url('/img/hero-pattern.svg')",
+      //   "footer-texture": "url('/img/footer-texture.png')",
+      // }),
     },
   },
   plugins: [],
